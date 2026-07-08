710546b076a80aaa194ba47046057cd0d3134945
karthick1121 <karthickvelraj11@gmail.com>
2026-07-08 21:27:58 +0530

    Create if_statement.py

---
+++ day03/if_statement.py
@@ -0,0 +1,51 @@
+print("====STUDENT ELIGIBILITY CHECKER====")
+name=(input("enter your name:"))
+age=int(input("enter your age:"))
+mark=int(input("enter your mark:"))         
+print("\n student details")
+print("Name:",name)        
+print("age:",age)
+print("mark:",mark)
+print("\n result")
+if age>=18:
+    print("eligible to vote") 
+if mark>=35:
+    print("PASS")
+if mark>=90:
+    print(" grade:A") 
+if mark>=70:
+    print(" verygood performance")
+if age>=18:
+    print("you are an adult") 
+
+print("thank you for using an student eligibility checker")                                                     
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+                 
