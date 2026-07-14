0e84f7b84ac00e28114e5462db936d17cec7f847
karthick1121 <karthickvelraj11@gmail.com>
2026-07-14 21:55:20 +0530

    Create while_loop.py

---
+++ day07/day07/while_loop.py
@@ -0,0 +1,19 @@
+print("\n1 enter 1 to 15")
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+print("\n2 sum of first N numbers:") 
+n=int(input("enter a number"))
+total=0
+while i<=n:
+    total=total+1
+    i=i+1   
+    print("sum=",total)
+print("\n3 factorial ")    
+i=1
+fact=1
+while i<=n:
+    fact=fact*1
+    i=i*1
+    print("factorial=",fact)
